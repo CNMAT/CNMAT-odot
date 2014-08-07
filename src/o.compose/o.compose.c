b8ea468a9da175d1fa8e567c8e95cdc7de3549d0
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2014-08-06 17:28:17 -0700

    trying to avoid sysmem_newptr calls

--- src/o.compose/o.compose.c
+++ src/o.compose/o.compose.c
@@ -257,16 +257,16 @@ void ocompose_newBundle(t_ocompose *x, t_osc_bndl_u *bu, t_osc_bndl_s *bs)
         x->have_new_data = 1;
         x->bndl_has_been_checked_for_subs = 0;
     
-        if (x->stored_bundle_data) {
-            sysmem_freeptr( x->stored_bundle_data );
-            x->stored_bundle_data = NULL;
-            x->stored_bundle_length = 0;
+        if (!x->stored_bundle_data) {
+            //sysmem_freeptr( x->stored_bundle_data );
+            //x->stored_bundle_data = NULL;
+            //x->stored_bundle_length = 0;
+            x->stored_bundle_data = sysmem_newptr( 1024 );
         }
-    
+        x->stored_bundle_length = 0;
         t_osc_bndl_s *b = x->bndl_s;
         long len = osc_bundle_s_getLen(b);
         char *ptr = osc_bundle_s_getPtr(b);
-        x->stored_bundle_data = sysmem_newptr( 1024 );
         x->stored_bundle_length = len;
         memcpy(x->stored_bundle_data, ptr, len);
     critical_exit(x->lock);
