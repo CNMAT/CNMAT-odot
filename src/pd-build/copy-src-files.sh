9a5623487d619fb9baf83a2d939b67971d2db224
Demo User <debian@beaglebone.(none)>
2015-01-15 20:11:44 -0800

    added -fPIC flag to linux target

--- src/pd-build/copy-src-files.sh
+++ src/pd-build/copy-src-files.sh
@@ -1,6 +1,6 @@
 # move all odot files into one place?
 
-COBJECT_LIST=(o.append o.atomize o.change o.collect o.cond o.context o.dict o.difference o.downcast o.explode o.expr o.expr.codebox o.flatten o.if o.intersection o.messageiterate o.listenumerate o.mappatch o.message o.pack o.pak o.prepend o.print o.printbytes o.route o.schedule o.select o.slip.decode o.slip.encode o.table o.timetag o.union o.unless o.validate o.var o.when)
+COBJECT_LIST=(o.compose o.display o.append o.atomize o.change o.collect o.cond o.context o.dict o.difference o.downcast o.explode o.expr o.expr.codebox o.flatten o.if o.intersection o.messageiterate o.listenumerate o.mappatch o.message o.pack o.pak o.prepend o.print o.printbytes o.route o.schedule o.select o.slip.decode o.slip.encode o.table o.timetag o.union o.unless o.validate o.var o.when)
 
 for f in ${COBJECT_LIST[*]}
 do
