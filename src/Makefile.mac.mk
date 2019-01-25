198525acfc88e266137fb844997f47a83a02d4a8
equilet <jml@dabkitsch.com>
2019-01-25 00:01:24 -0800

    updated C74 support path

--- src/Makefile.mac.mk
+++ src/Makefile.mac.mk
@@ -49,7 +49,7 @@ MFILES = $(foreach f, $(MOBJECT_LIST), $(f)/$(f).m)
 
 CODE_FILES = $(CFILES) $(MFILES)
 
-C74SUPPORT = ../../max6-sdk/c74support
+C74SUPPORT = ../../max-sdk/source/c74support
 MAX_INCLUDES = $(C74SUPPORT)/max-includes
 
 PLATFORM = MacOSX
