896938ed825f70f45e20ee921eb88f1670f4c66b
maccallum <john.m@ccallum.com>
2021-03-23 23:06:17 +0100

    improved make-release script and deleted old default-definitions folder

--- make-release.sh
+++ make-release.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 
-if [ $# = 0 ]
-then
-    echo "you must supply \"max\" or \"pd\" as an argument"
-    exit
-fi
+usage() {
+    echo "Usage: $0 -hmop -n [NAME]" 1>&2
+    echo "Clone a fresh copy of this folder, name it according to the options below," 1>&2
+    echo "and copy the built externals into the appropriate folders for release." 1>&2
+    echo "-h        : Help" 1>&2    
+    echo "-m        : Copy Max/MSP externals." 1>&2    
+    echo "-p        : Copy Pure Data externals." 1>&2
+    echo "-o        : Insert the name of the platform / OS into the filename." 1>&2
+    echo "-n [NAME] : Insert [NAME] into the filename." 1>&2
+}
 
-target=$1
+max=0
+pd=0
+os=0
+havename=0
+name=""
 
-if [ "$target" != "max" ] && [ "$target" != "pd" ]
-then
-    echo "argument must be \"max\" or \"pd\""
-    exit
-fi
+while getopts ":hmn:op" options; do
+    case "${options}" in
+	h)
+	    usage
+	    exit 0
+	    ;;
+	m)
+	    max=1
+	    ;;
+	p)
+	    pd=1
+	    ;;
+	o)
+	    os=1
+	    ;;
+	n)
+	    havename=1
+	    name=${OPTARG}
+	    ;;
+	:)
+	    usage
+	    exit 1
+	    ;;
+	\?)
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+# echo "max = $max pd = $pd os = $os name = $name"
+# exit
 
 require_clean_work_tree () {
     # Update the index
@@ -22,7 +59,7 @@ require_clean_work_tree () {
     # Disallow unstaged changes in the working tree
     if ! git diff-files --quiet --ignore-submodules --
     then
-	echo >&2 "cannot $1: you have unstaged changes."
+	echo >&2 "you have unstaged changes."
 	git diff-files --name-status -r --ignore-submodules -- >&2
 	err=1
     fi
@@ -30,7 +67,7 @@ require_clean_work_tree () {
     # Disallow uncommitted changes in the index
     if ! git diff-index --cached --quiet HEAD --ignore-submodules --
     then
-	echo >&2 "cannot $1: your index contains uncommitted changes."
+	echo >&2 "your index contains uncommitted changes."
 	git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
 	err=1
     fi
@@ -41,14 +78,24 @@ require_clean_work_tree () {
 	exit 1
     fi
 }
+
 require_clean_work_tree
 
-if [ $# = 2 ]
-then
-    platform=$2
-else
-    platform=`uname`
+filename=odot
 
+if [ $max = 1 ]
+then
+    filename="${filename}-Max"
+fi
+
+if [ $pd = 1 ]
+then
+    filename="${filename}-PD"
+fi
+
+if [ $os = 1 ]
+then
+    platform=`uname`
     platform=$(awk -vpl="$platform" 'BEGIN {
       pl=tolower(pl)
       if ( pl == "darwin" ){
@@ -59,21 +106,32 @@ else
         print "Linux"
       }
     }')
+    filename="${filename}-${platform}"
 fi
 
-filename=odot-$platform-`git describe --tags`
+if [ $havename = 1 ]
+then
+    filename="${filename}-${name}"
+fi
+
+version=`git describe --tags`
+
+filename="${filename}-${version}"
+
 git clone . ../$filename
 
-if [ "$target" = "max" ]
+rm -rf ../$filename/.git
+rm -rf ../$filename/src
+
+if [ max = 1 ]
 then
     cp -r externals ../$filename/
     cp -r dev/externals ../$filename/dev/
     cp -r deprecated/externals ../$filename/deprecated/
-elif [ "$target" = "pd" ]
+fi
+if [ pd = 1 ]
 then 
     cp -r pd/*.pd_* ../$filename/pd/
     cp -r pd/dev/*.pd_* ../$filename/pd/dev/
     cp -r pd/deprecated/*.pd_* ../$filename/pd/deprecated/
 fi
-
-cd .. && tar zcvf $filename.tgz $filename
